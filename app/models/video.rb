2b8867f53486bc28d56694dcf767b914af228f5c
Mathew Wilson <daicoden@copypastel.com>
2008-11-04 08:26:16 -0500

    Cleaned up videos a bit.  Though the proc for the format of the URL was bad ass it was confusing to validate in two places, sorry Ecin!  Also added a database template

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,22 +1,9 @@
 class Video
   include DataMapper::Resource
-  
   @@validation_url = 'http://gdata.youtube.com/feeds/api/videos/'
   
   property :id, Serial
-  property :url, Text, :lazy => false, :format => Proc.new {|str|
-    unless str.nil?
-      url = @@validation_url + str.gsub(/.*youtube.com\/watch\?v=/, '').gsub(/&.*/, '')
-      result = Net::HTTP.get(Module::URI.parse(url))
-      if result == 'Invalid id'
-        false
-      else
-        true
-      end
-    else
-      false
-    end
-    }, :message => "check video url"
+  property :url, Text, :lazy => false
   property :title, Text, :lazy => false, :default => "Pending..."
   property :created_at, DateTime
   property :updated_at, DateTime
@@ -24,28 +11,34 @@ class Video
   property :count, Integer, :default => 0
   property :thumbnail, String, :nullable => :false
   
+  class << self
+    def list
+      self.all :order => [:updated_at.desc]
+    end
+  end
+  
   before :valid? do
     validation_url = @@validation_url + self.url.gsub(/.*youtube.com\/watch\?v=/, '').gsub(/&.*/, '')
     result = Net::HTTP.get(Module::URI.parse(validation_url))
+    
+    self.errors.add(:url, "Check video url.") and throw :halt if result == 'Invalid id'
+    
     parser = Hpricot(result)
     thumbnail = (parser/'media:thumbnail')[1]
     title = (parser/'media:title').first
-    unless (parser/'yt:noembed').size.zero?
-      self.errors.add(:url, "video isn't embeddable")
+    embedable_tag = (parser/'yt:noembed')
+    
+    self.errors.add(:url, 'Video is not embeddable.') and throw :halt unless embedable_tag.empty?
+    #Makes sure program does not crash if youtube changes XML format
+    begin
+      self.thumbnail = thumbnail[:url]
+      self.title = title.inner_html
+    rescue
+      self.errors.add(:url, 'Video has improper format.')
       throw :halt
     end
-    if thumbnail
-      self.thumbnail = thumbnail[:url]
-    end
-    if title
-      self.title = title.inner_html
-    end
   end
-  
-  def self.list
-    self.all :order => [:updated_at.desc]
-  end
-  
+    
   def next
     Video.first :updated_at.gt => self.updated_at, :order => [:updated_at.asc]
   end
