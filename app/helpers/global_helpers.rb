7d0ad8e54a81f4a499e57d34516093600a15a57d
daicoden <daicoden@copypastel.com>
2009-03-29 06:31:08 -0600

    Ecin needs to look at the commented area, added this so the massive changes from the previous commit wouldn't overwhelm him.

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -2,6 +2,8 @@ module Merb
   module GlobalHelpers
     # helpers defined here available to all views.  
     def video_frame_around(html)
+      # TODO: GET ECIN TO LOOK AT THIS!!!  For some reason the frame spans all the way to the right for the thumnails
+      # I haven't tried with multiple soundtracks added, just 1.  Also I did update it for our versions of merb so pulling should work off the bat.  -Dai
       "<div style=\"       background: url('images/frame_horizontal_top.png')    repeat-x top left;    width: 100%; height: 100%; position: relative;\">
           <div style=\"    background: url('images/frame_horizontal_bottom.png') repeat-x bottom left; width: 100%; height: 100%; position: relative;\">
             <div style=\"  background: url('images/frame_vertical.png')          repeat-y top left;    width: 100%; height: 100%; position: relative;\">
