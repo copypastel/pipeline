00d3cb9a1ccac2d7210e2d9e0e72b57caa3a7d9a
daicoden <daicoden@copypastel.com>
2009-03-29 05:24:06 -0600

    Commiting new files for latest version of merb

--- config/init.rb
+++ config/init.rb
@@ -11,23 +11,12 @@ Merb::Config.use do |c|
   c[:session_store] = 'cookie'  # can also be 'memory', 'memcache', 'container', 'datamapper
   
   # cookie session store configuration
-  c[:session_secret_key]  = 'cd264ce8cccec9f0094a22b80ecc6bb5a4cdc8d9'  # required for cookie session store
-  # c[:session_id_key] = '_session_id' # cookie session id key, defaults to "_session_id"
+  c[:session_secret_key]  = '3e12af6a3faddc12c9ce12e32d5f370a9deec98d'  # required for cookie session store
+  c[:session_id_key] = '_temp_session_id' # cookie session id key, defaults to "_session_id"
 end
  
 Merb::BootLoader.before_app_loads do
   # This will get executed after dependencies have been loaded but before your app's classes have loaded.
-  module Merb
-    module Assets
-      module AssetHelpers
-        ASSET_FILE_EXTENSIONS = {
-          :javascript => ".js",
-          :stylesheet => ".css",
-          :image => ".png"
-        }
-      end
-    end
-  end
 end
  
 Merb::BootLoader.after_app_loads do
